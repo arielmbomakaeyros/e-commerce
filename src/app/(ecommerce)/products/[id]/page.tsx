bf49c79532fefba1b22d900bbbe6cb34f5501cd2
arielmbomakaeyros <ariel.mboma@kaeyros-analytics.com>
2024-04-02 02:34:47 +0100

    inplemented url state handling with useParam

--- src/app/(ecommerce)/products/[id]/page.tsx
+++ src/app/(ecommerce)/products/[id]/page.tsx
@@ -1,11 +1,46 @@
+import { ProductType } from '@/lib/products';
+import { useSearchParams } from 'next/navigation';
 import React from 'react'
 
-const ProductDetailsPage = () => {
+const ProductDetailsPage = ({ params, searchParams }: {
+  params: {
+    id: string | number
+  },
+  searchParams: ProductType
+}) => {
+
+  // console.log("searchParams?.namesearchParams?.name", searchParams)
   return (
-    <div>
-      hello
+    <div className='flex min-h-screen flex-col items-center justify-start gap-[3rem] p-24'>
+      { params?.id } { searchParams?.name}
     </div>
   )
 }
 
 export default ProductDetailsPage
+
+
+
+
+
+
+
+// "use client"
+// import { ProductType } from '@/lib/products';
+// import { useSearchParams } from 'next/navigation';
+// import React from 'react'
+
+// const ProductDetailsPage = () => {
+//   const searchParams = useSearchParams (); 
+//   // const newParam = searchParams.get("new")
+//   console.log("...................", searchParams?.get("id"))
+//   // console.log("searchParams?.namesearchParams?.name", params?.name, params?.id, params?.price)
+//   return (
+//     <div className='flex min-h-screen flex-col items-center justify-start gap-[3rem] p-24'>
+
+//       { searchParams?.get("id") } { searchParams?.get("name")}
+//     </div>
+//   )
+// }
+
+// export default ProductDetailsPage
